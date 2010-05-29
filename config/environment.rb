aac357b33af50938ce278d72734eb6c091145ea1
Brian Scott <brainscott@gmail.com>
2010-05-28 17:04:25 -0700

    Started Changing Active Record for MongoDB

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ app_name = "rubyflow"
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -22,6 +22,9 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use (only works if using vendor/rails).
   # To use Rails without a database, you must remove the Active Record framework
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [:active_record]
+  config.gem 'mongomapper', :version => '>= 0.2.1'
+  
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
---
+++ config/initializers/mongo_mapper.rb
@@ -0,0 +1,3 @@
+MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27062, { :logger => Rails.logger })
+MongoMapper.database = 'mongoflow'
+MongoMapper.database.authenticate('mongoflow', 'm0ng0')
