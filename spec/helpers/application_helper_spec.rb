781497ca93819a48e0a2ec20842b27cece82d76f
Alex Sharp <ajsharp@gmail.com>
2010-07-10 23:42:43 -0700

    light refactor of #form_errors specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  class MockView < ActionView::Base; include ApplicationHelper; end
+  class MockView < ActionView::Base
+    include ApplicationHelper
+  end
   
   describe '#form_errors' do
     class FakeItem
@@ -10,20 +12,24 @@ describe ApplicationHelper do
       field :title
       validates_presence_of :title
       validates_length_of   :title, :minimum => 8
-
-      def save; false; end
     end
 
-    before do
-      @template = MockView.new
-      @item     = FakeItem.new
+    before(:each) { @template = MockView.new }
+    after(:all)   { Item.destroy_all         }
+
+    it 'should display multiple errors hash in a list' do
+      item = Item.create
+      msg1 = item.errors.full_messages[0]
+      msg2 = item.errors.full_messages[1]
+
+      @template.form_errors(item).should == "<ul class=\"errorExplanation\"><li>#{msg1}</li><li>#{msg2}</li></ul>"
     end
 
-    it 'should display the errors hash in a <ul>' do
-      @item.valid?
-      msg1 = @item.errors.full_messages[0]
-      msg2 = @item.errors.full_messages[1]
-      @template.form_errors(@item).should == "<ul class=\"errorExplanation\"><li>#{msg1}</li><li>#{msg2}</li></ul>"
+    it 'should display one error in a list' do
+      item = Item.create :title => "2short"
+      msg = item.errors.full_messages[0]
+
+      @template.form_errors(item).should == "<ul class=\"errorExplanation\"><li>#{msg}</li></ul>"
     end
 
   end
