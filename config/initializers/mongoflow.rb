e227041bb76654919c621cbfa2d10de21e567a11
Umang Chouhan + Alex Sharp <ajsharp+uc+as@gmail.com>
2010-07-10 20:09:24 -0700

    use Rails.root to avoid deprecation warning

--- config/initializers/mongoflow.rb
+++ config/initializers/mongoflow.rb
@@ -1,2 +1,2 @@
 app_name = "mongoflow"
-APP_CONFIG = YAML::load(File.open("#{RAILS_ROOT}/config/#{app_name}.yml")).symbolize_keys
+APP_CONFIG = YAML::load(File.open("#{Rails.root}/config/#{app_name}.yml")).symbolize_keys
