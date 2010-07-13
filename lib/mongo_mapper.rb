f8e3ad2dabfb58cea94c906d2a7132873c9108a8
Brian Scott <brainscott@gmail.com>
2010-07-12 17:34:14 -0700

    Modified Sidebar data

--- lib/mongo_mapper.rb
+++
@@ -1,3 +0,0 @@
-#MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27062, { :logger => Rails.logger })
-#MongoMapper.database = 'mongoflow'
-#MongoMapper.database.authenticate('mongoflow', 'm0ng0')
