651428d4d28cccdd533648baf9dbd9878fa521c4
Brian Scott <brainscott@gmail.com>
2010-07-17 00:15:16 -0700

    Added email field to mongoid and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,20 @@ class User
          :recoverable, :rememberable, :trackable, :validatable
 
   field :login
+  field :email
   field :remember_token_expires_at
 
  #validates_presence_of :login
  #validates_uniqueness_of :name, :email, :case_sensitive => false
 
-  validates_presence_of     :login #, :email
+  validates_presence_of     :login 
   validates_presence_of     :password,                   :if => :password_required?
   validates_presence_of     :password_confirmation,      :if => :password_required?
   validates_length_of       :password, :within => 4..40, :if => :password_required?
   validates_confirmation_of :password,                   :if => :password_required?
   validates_length_of       :login,    :within => 3..40
-  #validates_length_of       :email,    :within => 3..100
+  validates_length_of       :email,    :within => 3..100
+  validates_uniqueness_of :email
   validates_uniqueness_of   :login, :case_sensitive => false
   validates_format_of       :login, :with => /^\w+$/
   validates_format_of       :url, :with => /^(|http\:\/\/.*)$/
